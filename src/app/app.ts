4b80748f430f9a17455fcea11ef792bab0fee462
Ilya <iosdsbb@gmail.com>
2025-11-11 11:18:44 +0300

    refactor: app component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,25 +1,13 @@
-import {Component, inject, signal} from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
 import {ProfileCard} from './common-ui/profile-card/profile-card';
-import {ProfileService} from './data/services/profile.service';
-import {JsonPipe} from '@angular/common';
-import {Profile} from './data/interfaces/profile.interface';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ProfileCard],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  profileService = inject(ProfileService);
 
-  profiles: Profile[] = [];
-
-  constructor() {
-    this.profileService.getTestAccounts()
-      .subscribe(val => {
-        this.profiles = val;
-      });
-  }
 }
