81060f52a1d90b407db4bfdfa0e8827b1a9f8dbc
Ilya <iosdsbb@gmail.com>
2025-11-11 19:48:10 +0300

    chore: playing with RxJS

--- src/app/pages/login-page/login-page.ts
+++ src/app/pages/login-page/login-page.ts
@@ -1,6 +1,7 @@
 import {Component, inject} from '@angular/core';
 import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 import {AuthService} from '../../auth/auth.service';
+import {from, map, skip, take} from 'rxjs';
 
 @Component({
   selector: 'app-login-page',
@@ -23,6 +24,20 @@ export class LoginPage {
       nonNullable: true
     }),
   });
+
+  constructor() {
+    from([1,2,3,4,5,6,7,8,9])
+      .pipe(
+        map(val => {
+          return val * 2;
+        }),
+        skip(2),
+        take(5),
+      )
+      .subscribe(val => {
+        console.log(val);
+      })
+  }
 
   onSubmit() {
     if (this.form.valid) {
