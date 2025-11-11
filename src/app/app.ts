f9c07980522747e08c10898c96515eaa28a47ee4
Ilya <iosdsbb@gmail.com>
2025-11-11 10:43:06 +0300

    feat: updated app, show profiles

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,9 @@
-import { Component, signal } from '@angular/core';
+import {Component, inject, signal} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {ProfileCard} from './common-ui/profile-card/profile-card';
+import {ProfileService} from './data/services/profile.service';
+import {JsonPipe} from '@angular/common';
+import {Profile} from './data/interfaces/profile.interface';
 
 @Component({
   selector: 'app-root',
@@ -9,5 +12,14 @@ import {ProfileCard} from './common-ui/profile-card/profile-card';
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('hello-angular');
+  profileService = inject(ProfileService);
+
+  profiles: Profile[] = [];
+
+  constructor() {
+    this.profileService.getTestAccounts()
+      .subscribe(val => {
+        this.profiles = val;
+      });
+  }
 }
