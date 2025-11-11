e1325db8853886e87debb38938d31516627200bc
Ilya <iosdsbb@gmail.com>
2025-11-11 10:56:58 +0300

    feat: show profile avatar

--- src/app/common-ui/profile-card/profile-card.ts
+++ src/app/common-ui/profile-card/profile-card.ts
@@ -1,9 +1,12 @@
 import {Component, Input} from '@angular/core';
 import {Profile} from '../../data/interfaces/profile.interface';
+import {ImgUrlPipe} from '../../helpers/pipes/img-url-pipe';
 
 @Component({
   selector: 'app-profile-card',
-  imports: [],
+  imports: [
+    ImgUrlPipe
+  ],
   templateUrl: './profile-card.html',
   styleUrl: './profile-card.scss',
 })
