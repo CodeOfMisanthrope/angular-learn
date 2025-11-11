600e6d70c5dad2a31b13c205cc230dcba008cd9a
Ilya <iosdsbb@gmail.com>
2025-11-11 18:33:56 +0300

    feat: updated login-page, added submit form

--- src/app/pages/login-page/login-page.ts
+++ src/app/pages/login-page/login-page.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {AuthService} from '../../auth/auth.service';
 
 @Component({
   selector: 'app-login-page',
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './login-page.html',
   styleUrl: './login-page.scss',
 })
 export class LoginPage {
+  authService = inject(AuthService);
 
+  form = new FormGroup({
+    username: new FormControl("", {
+      validators: Validators.required,
+      nonNullable: true
+    }),
+    password: new FormControl("", {
+      validators: Validators.required,
+      nonNullable: true
+    }),
+  });
+
+  onSubmit() {
+    if (this.form.valid) {
+      console.log(this.form.value);
+      this.authService.login(this.form.value)
+        .subscribe()
+    }
+  }
 }
