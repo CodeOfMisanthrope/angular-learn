5e3179e6a20ab45c4dad500e5e98e31a37c3c4c4
Ilya <iosdsbb@gmail.com>
2025-11-11 11:18:24 +0300

    feat: added search page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import {LoginPage} from './pages/login-page/login-page';
+import {SearchPage} from './pages/search-page/search-page';
 
 export const routes: Routes = [
-  {path: 'component', component: LoginPage},
+  {path: '', component: SearchPage},
+  {path: 'login', component: LoginPage},
 ];
