6d9404fc6afec65fdc6386f06a269bdd6e9280eb
Ilya <iosdsbb@gmail.com>
2025-11-12 14:26:27 +0300

    feat: added logo and nav to sidebar

--- src/app/common-ui/sidebar/sidebar.ts
+++ src/app/common-ui/sidebar/sidebar.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import {SvgIcon} from '../svg-icon/svg-icon';
+import {NgForOf} from '@angular/common';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [],
+  imports: [
+    SvgIcon,
+    NgForOf
+  ],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.scss',
 })
 export class Sidebar {
-
+ menuItems = [
+   {
+     label: 'Моя страница',
+     icon: 'home',
+     link: ''
+   },
+   {
+     label: 'Чаты',
+     icon: 'chat',
+     link: 'chats'
+   },
+   {
+     label: 'Поиск',
+     icon: 'search',
+     link: 'search'
+   },
+ ];
 }
